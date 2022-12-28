640a4891a994a2b10fc56e072b150ea8aa4cc814
[Carl Griffin] <[griffiniiicarl@gmail.com]>
2022-12-28 16:36:12 -0600

    initial commit

---
+++ src/main/java/net/yorksolutions/fafoshop/FafoShopApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.fafoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FafoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FafoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yorksolutions/fafoshop/FafoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.fafoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FafoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
