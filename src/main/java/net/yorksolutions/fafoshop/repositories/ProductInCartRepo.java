6aa9fd66bdc4aba92c3b51e39c2175289e8b5deb
Bryan Turner <bryan.turner.ch@gmail.com>
2022-12-30 14:48:50 -0700

    prod in cart bug fix, http file added

--- src/main/java/net/yorksolutions/fafoshop/repositories/ProductInCartRepo.java
+++ src/main/java/net/yorksolutions/fafoshop/repositories/ProductInCartRepo.java
@@ -1,8 +1,9 @@
 package net.yorksolutions.fafoshop.repositories;
 
+import net.yorksolutions.fafoshop.models.ProductInCart;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductInCartRepo extends CrudRepository {
+public interface ProductInCartRepo extends CrudRepository<ProductInCart, Long> {
 }
