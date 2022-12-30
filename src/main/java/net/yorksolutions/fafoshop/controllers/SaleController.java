af27dc8950d9f221e56a13873f8b8669a165e241
Bryan Turner <bryan.turner.ch@gmail.com>
2022-12-30 11:16:41 -0700

    sale files created

---
+++ src/main/java/net/yorksolutions/fafoshop/controllers/SaleController.java
@@ -0,0 +1,60 @@
+package net.yorksolutions.fafoshop.controllers;
+
+import net.yorksolutions.fafoshop.models.Sale;
+import net.yorksolutions.fafoshop.services.SaleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/sales")
+public class SaleController {
+
+    private  final SaleService service;
+
+    public SaleController(SaleService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Iterable<Sale> getAllSales() {
+        return service.getAllSales();
+    }
+
+    @GetMapping("/{id}")
+    public Sale getSaleById(@PathVariable Long id) {
+        try {
+            return service.getSaleById(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public void createSale(@RequestBody Sale sale) {
+        try {
+            service.createSale(sale);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSaleById(@PathVariable Long id) {
+        try {
+            service.deleteSaleById(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public void modifySale(@PathVariable Long id, @RequestBody Sale sale) {
+        try {
+            service.modifySale(id, sale);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
