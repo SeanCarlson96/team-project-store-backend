98508c899483dc14c8d0f1140f193b732bc17653
Bryan Turner <bryan.turner.ch@gmail.com>
2023-01-05 22:31:12 +0100

    sale dto added to sale service/controller

--- src/main/java/net/yorksolutions/fafoshop/services/SaleService.java
+++ src/main/java/net/yorksolutions/fafoshop/services/SaleService.java
@@ -1,12 +1,16 @@
 package net.yorksolutions.fafoshop.services;
 
+import net.yorksolutions.fafoshop.DTOs.ProductDTO;
+import net.yorksolutions.fafoshop.DTOs.SaleDTO;
 import net.yorksolutions.fafoshop.models.Product;
 import net.yorksolutions.fafoshop.models.Sale;
 import net.yorksolutions.fafoshop.repositories.ProductRepo;
 import net.yorksolutions.fafoshop.repositories.SaleRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class SaleService {
@@ -27,32 +31,39 @@ public class SaleService {
         return saleRepo.findById(id).orElse(null);
     }
 
-    public void createSale(Sale saleRequest) throws Exception {
-        if (saleRepo.findSaleBySaleName(saleRequest.getSaleName()).isPresent())
+    public void createSale(SaleDTO saleRequest) throws Exception {
+        if (saleRepo.findSaleBySaleName(saleRequest.saleName).isPresent())
             throw new Exception("Sale name already exists");
 
         Sale sale = new Sale();
-        sale.setSaleName(saleRequest.getSaleName());
-        sale.setStartDate(saleRequest.getStartDate());
-        sale.setStopDate(saleRequest.getStopDate());
-        sale.setProducts(saleRequest.getProducts());
-        sale.setDiscountPercentage(saleRequest.getDiscountPercentage());
+        Set<Product> productSet = new HashSet<>();
+        sale.setSaleName(saleRequest.saleName);
+        sale.setStartDate(saleRequest.startDate);
+        sale.setStopDate(saleRequest.stopDate);
+        sale.setDiscountPercentage(saleRequest.discountPercentage);
 
-        // TODO - post sale logic
+        for (ProductDTO product: saleRequest.products) {
+            Optional<Product> productOptional = productRepo.findById(product.id.get());
+            if (productOptional.isEmpty())
+                throw new Exception();
 
-        saleRepo.save(sale);
+            productSet.add(productOptional.get());
+        }
 
-        for (Product saleProduct: sale.getProducts()) {
+        sale.setProducts(productSet);
+        Sale savedSale = saleRepo.save(sale);
+
+        for (Product saleProduct: savedSale.getProducts()) {
             Optional<Product> productOptional = productRepo.findById(saleProduct.getId());
             if (productOptional.isEmpty())
                 throw new Exception("Product id does not exist");
 
             Product product = productOptional.get();
-            product.setSale(sale);
+            product.setSale(savedSale);
             productRepo.save(product);
         }
     }
-
+// sale dto in dto , update prod dto
     public void deleteSaleById(Long id) throws Exception {
         if (saleRepo.findById(id).isEmpty())
             throw new Exception();
